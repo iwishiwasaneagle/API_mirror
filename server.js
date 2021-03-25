2372c3c8550300dbef4c0acb3d1a47a86b53a505
iwishiwasaneagle <jh.ewers@gmail.com>
2021-03-25 17:25:26 +0000

    Minor changes

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const returner = (req) => ({
-    
+const logger = (req,res,next) => {
+    console.log("Call to ", req.baseUrl + req.path);
+    console.log("   Body:   ", req.body);
+    console.log("   Params: ", req.params);
+    console.log("   Method: ", req.method);
+    console.log("   Headers:", req.headers);
+    next();
+}
+app.use(logger);
 
+const returner = (req) => ({
     "body":req.body,
-    "query":req.params.p,
+    "query":req.params?.p || "",
     "headers":req.headers
 });
+
 
 
 app.get('/:p',(req,res)=>res.send(returner(req)));
@@ -24,6 +33,15 @@ app.post('/:p',(req,res)=>res.send(returner(req)));
 app.put('/:p',(req,res)=>res.send(returner(req)));
 
 app.delete('/:p',(req,res)=>res.send(returner(req)));
+
+app.get('/',(req,res)=>res.send(returner(req)));
+
+app.post('/',(req,res)=>res.send(returner(req)));
+
+app.put('/',(req,res)=>res.send(returner(req)));
+
+app.delete('/',(req,res)=>res.send(returner(req)));
+
 
 PORT = 3000;
 app.listen(PORT, ()=>{
